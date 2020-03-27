8c2ad0a1e977581a8f480cbe026d959631898966
Andrew Durrant <andrew.durrant75@gmail.com>
2020-03-27 07:37:33 -0600

    bug fix in server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const { PORT } = require('/config');
+const { PORT } = require('./config');
 
 app.listen(PORT, () => {
   console.log(`Server listening in ${process.env.NODE_ENV} mode at http://localhost:${PORT}`);
